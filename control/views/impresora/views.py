0a2d4a6e47f59ecb97b691cf5b5e8aa91b8034e8
nestormarlier <47480842+nestormarlier@users.noreply.github.com>
2021-01-01 12:49:06 -0300

    ajax para eliminar los registros

--- control/views/impresora/views.py
+++ control/views/impresora/views.py
@@ -100,10 +100,22 @@ class ImpresoraDeleteView(DeleteView):
     template_name = 'impresora/delete.html'
     success_url = reverse_lazy('impresora_list')
 
+    #@method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            self.object.delete()
+        except Exception as e:
+            data['error']= str(e)
+        return JsonResponse(data)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Eliminación de una impresora'
         context['entidad'] = 'Impresora'
         context['list_url'] = self.success_url
-        # context['accion'] = 'delete' no usare ajax para la baja
         return context
